dfc48135e323873437a939794d4cb94a9cf0b205
aksbrar <aksbrar34@gmail.com>
2025-08-05 23:56:35 -0400

    tabes created

--- create-tables.sql
+++ create-tables.sql
@@ -39,3 +39,37 @@ CREATE TABLE IF NOT EXISTS dealerships (
   state CHAR(2) NOT NULL,
   established DATE NOT NULL
 );
+
+/*
+	Create the staff table
+		Include these columns:
+			id - serial primary key
+			dealership_id - INTEGER NOT NULL REFERENCES dealerships(id)
+			name - text - NOT NULL
+			role - text - NOT NULL
+*/
+
+CREATE TABLE IF NOT EXISTS staff (
+  id SERIAL PRIMARY KEY,
+  dealership_id INT NOT NULL REFERENCES dealerships(id),
+  name TEXT NOT NULL,
+  role TEXT NOT NULL
+);
+
+/*
+	Create the sold_cars table
+		Include these columns:
+			id - serial primary key - NOT NULL
+			cars_id - integer - NOT NULL - foreign key referencing cars(id)
+			seller - integer - NOT NULL - foreign key referencing staff(id)
+			sold_date DATE - NOT NULL
+			sold_price INTEGER - NOT NULL
+*/
+
+CREATE TABLE IF NOT EXISTS sold_cars (
+  id SERIAL PRIMARY KEY,
+  cars_id INTEGER NOT NULL REFERENCES cars(id),
+  seller INTEGER NOT NULL REFERENCES staff(id),
+  sold_date DATE NOT NULL,
+  sold_price INTEGER NOT NULL
+);
