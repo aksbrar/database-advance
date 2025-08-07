cc4054d429581c8b387c6cf991b4aa465c0ed080
aksbrar <aksbrar34@gmail.com>
2025-08-07 17:39:40 -0400

    joins almost done

--- query.sql
+++ query.sql
@@ -1,3 +1,3 @@
-SELECT brand, model, price, sold_price
-  FROM sold_cars SC
-   LEFT JOIN cars C ON SC.cars_id = C.id;
+SELECT name, role, dealership_id, city
+  FROM staff S
+  LEFT JOIN dealerships D ON S.dealership_id = D.id;
