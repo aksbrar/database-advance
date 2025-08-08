8c7c0ee5502a4b0b974da0609ca5bd4b9b548b24
aksbrar <aksbrar34@gmail.com>
2025-08-08 12:10:31 -0400

    multiple tables join

--- query.sql
+++ query.sql
@@ -1,3 +1,23 @@
-SELECT name, role, dealership_id, city
-  FROM staff S
-  LEFT JOIN dealerships D ON S.dealership_id = D.id;
+/*
+	Show the city and state of dealerships
+		with a count of the cars sold
+		aliased as cars_sold
+		
+	Select from sold_cars
+		join with the relevant tables
+		
+	Include dealerships which have no sold cars
+	
+	Order the count in descending order
+		
+	Hint: you may need to join using a table not included in our columns
+*/
+
+SELECT 
+  D.city,
+  D.state,
+  COUNT(seller)
+FROM sold_cars SC
+  LEFT JOIN staff S ON SC.seller = S.id
+  FULL JOIN dealerships D ON S.dealership_id = D.id
+  GROUP BY D.city, D.state;
